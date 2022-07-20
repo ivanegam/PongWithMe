483cdc3589780796b29728ab0a47f0314d03c004
Tyler Walker <tgwalker93@gmail.com>
2022-07-19 22:44:26 -0600

    minor update

--- src/main/java/pwm/pongwithme/InGameObject.java
+++ src/main/java/pwm/pongwithme/InGameObject.java
@@ -2,9 +2,4 @@ package pwm.pongwithme;
 
 public abstract class InGameObject
 {
-    public float position_x;
-    public float position_y;
-    public int currentTime;
-    public String image;
-
 }
